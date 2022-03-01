d10216a73faac6436b50b7ddf5a485b83a1b15e7
NoobNobody <83828105+NoobNobody@users.noreply.github.com>
2022-03-01 16:22:50 +0100

    Add files via upload

---
+++ WindowsFormsApp1/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                if (textBox1.Text == "" || textBox2.Text == "")
+                    MessageBox.Show("Uzupełnij wszystkie pola!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else if (textBox1.Text == "admin" && textBox2.Text == "admin")
+                {
+                    MainForm main = new MainForm();
+                    main.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Podałeś nieprawidłowe dane! Spróbuj ponownie.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
